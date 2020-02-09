0ed2001c261b2eafb497e7712e02c90c118860c2
Rui Moreira <ruipmoreira25@gmail.com>
2020-02-09 22:16:59 +0000

    add build folder

---
+++ build/precache-manifest.b21133035723440bc9c755047098e0da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bbfd4f86c4c03683b36cc30cbab802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc5e59f93d8fd026e0b5",
+    "url": "/static/css/main.0b44c97b.chunk.css"
+  },
+  {
+    "revision": "1265f00d156e7560eb4b",
+    "url": "/static/js/2.60f628e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.60f628e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc5e59f93d8fd026e0b5",
+    "url": "/static/js/main.0497f3e6.chunk.js"
+  },
+  {
+    "revision": "9aedf1ef7999bbd8dbd5",
+    "url": "/static/js/runtime-main.01ad01f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
